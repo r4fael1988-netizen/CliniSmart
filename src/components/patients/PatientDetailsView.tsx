022320ce702f1f4a0060c20949b32123af0cfe66
RN Consorcios <contato@rnconsorcios.com>
2026-04-09 10:07:44 -0400

    fix: corrected syntax error in PatientDetailsView and retrying deploy

--- src/components/patients/PatientDetailsView.tsx
+++ src/components/patients/PatientDetailsView.tsx
@@ -426,6 +426,7 @@ export function PatientDetailsView({
                      )}
                   </tbody>
                </table>
+              </div>
             </div>
          )}
          
